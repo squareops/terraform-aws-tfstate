188923796dbaabbe79fd75d95bf2c7e133af2f9b
yuvraj.singh <yuvraj.singh@revnue.com>
2023-09-05 19:27:57 +0530

    added chnages for enabling cloudwatch logging option and updated kms key policy

--- examples/state-storage-backend/main.tf
+++ examples/state-storage-backend/main.tf
@@ -1,8 +1,8 @@
 locals {
-  region      = "us-east-1"
+  region      = "us-east-2"
   environment = "prod"
   additional_tags = {
-    Owner      = "Organization_Name"
+    Owner      = "organization_name"
     Expires    = "Never"
     Department = "Engineering"
   }
@@ -13,7 +13,7 @@ module "backend" {
   logging            = true
   cloudwatch_logging = true
   environment        = local.environment
-  bucket_name        = "production-tfstate-bucket-personel" #unique global s3 bucket name
+  bucket_name        = "production-tfstate-bucket" #unique global s3 bucket name
   force_destroy      = true
   versioning_enabled = true
 }
